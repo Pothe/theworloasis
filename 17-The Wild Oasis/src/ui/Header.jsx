8e526b79a1e6078f0a3819fc3950a26f5304c640
Pothe <keoutpothe2014@gmail.com>
2025-12-12 16:43:31 +0700

    border

--- 17-The Wild Oasis/src/ui/Header.jsx
+++ 17-The Wild Oasis/src/ui/Header.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 const StyleHeader= styled.header`
     background-color:var(--color-grey-0);
-    padding: 2rem 2rem;
-    border-bottom: 1px solid var(--color-grey-100);
+    /* padding: 2rem 2rem;
+    border-bottom: 1px solid var(--color-grey-100); */
 `
 function Header() {
     return (
