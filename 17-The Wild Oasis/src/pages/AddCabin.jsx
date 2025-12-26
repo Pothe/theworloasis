514ed3d23042afa8bc45b56bf1fa26f247bda0eb
Pothe <keoutpothe2014@gmail.com>
2025-12-26 10:34:00 +0700

    contextApi

--- 17-The Wild Oasis/src/pages/AddCabin.jsx
+++ 17-The Wild Oasis/src/pages/AddCabin.jsx
@@ -1,37 +1,35 @@
-import { useState } from "react"
-import Buttton from "../ui/Button"
+import Button from "../ui/Button"
 import Modal from '../ui/Modal'
 import CreateCabinForm from "../features/cabins/CreateCabinForm"
+import CabinTable from "../features/cabins/CabinTable"
 
-// function AddCabin(){
-//     return
-//     <Modal>
-//         <Modal.Open opens="cabin-form">
-//             <Buttton>Add New</Buttton>
-//         </Modal.Open>
-//         <Modal.Window name="cabin-form">
-//             <CreateCabinForm/>
-//         </Modal.Window>
+function AddCabin(){
+    return <Modal>
+        <Modal.Open opens="show-cabin-form">
+            <Button>Add New</Button>
+        </Modal.Open>
+        <Modal.Window name="show-cabin-form">
+            <CreateCabinForm/>
+        </Modal.Window>
 
-// {/* 
-//          <Modal.Open opens="table">
-//             <Buttton>Show Table</Buttton>
-//         </Modal.Open>
-//         <Modal.Window name="table">
-//             <CreateCabinForm/>
-//         </Modal.Window> */}
-//     </Modal>
-
-// }
-function AddCabin() {
-    const [isOpen, setisOpen] = useState(false)
-    return (
-        <div>
-            <Buttton bg="primary" size="small" onClick={()=>setisOpen(true)}>Add</Buttton>
-         {isOpen && <Modal onClose ={()=>setisOpen(false)}><CreateCabinForm onCloseModal={()=>setisOpen(false)}/></Modal>}
-        </div>
-    )
+        <Modal.Open opens="table">
+            <Button>show table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+           <CabinTable/>
+        </Modal.Window>
+    </Modal>
 }
+
+// function AddCabin() {
+//     const [isOpen, setisOpen] = useState(false)
+//     return (
+//         <div>
+//             <Buttton bg="primary" size="small" onClick={()=>setisOpen(true)}>Add</Buttton>
+//          {isOpen && <Modal onClose ={()=>setisOpen(false)}><CreateCabinForm onCloseModal={()=>setisOpen(false)}/></Modal>}
+//         </div>
+//     )
+// }
 
 export default AddCabin
 
