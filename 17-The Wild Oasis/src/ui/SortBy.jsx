5e53d9e5c7f575b57f7a006979a5e76cd15957c6
Pothe <keoutpothe2014@gmail.com>
2026-02-19 14:40:55 +0700

    Create SortBy.jsx

---
+++ 17-The Wild Oasis/src/ui/SortBy.jsx
@@ -0,0 +1,20 @@
+import { useSearchParams } from "react-router-dom"
+import Select from "./Select"
+
+
+
+function SortBy({options}) {
+    const [searchParams,setSearchParams] = useSearchParams()
+
+    function handleClick(e){
+        searchParams.set("sortBy", e.target.value)
+        setSearchParams(searchParams)
+
+
+    }
+    return (
+        <Select options={options} type="white" onChanged={handleClick}/>
+    )
+}
+
+export default SortBy
