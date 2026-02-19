b89cc709407a750524d368834a63bfedf15746bd
Pothe <keoutpothe2014@gmail.com>
2026-02-19 17:09:38 +0700

    Update useBooking.js

--- 17-The Wild Oasis/src/features/bookings/useBooking.js
+++ 17-The Wild Oasis/src/features/bookings/useBooking.js
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 export function useBooking(){
+    const [searchParams]= useSearchParams()
+    const fillterValue = searchParams.get("status")
+    console.log("useBooking",fillterValue)
     const {data:Booking,error:hasError,isLoading}=useQuery({
         queryKey:["bookings"],
         queryFn:getBookings
