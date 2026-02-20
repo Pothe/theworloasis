4280f97d7def6acb70401a930c07bcd46ac6d4bf
Pothe <keoutpothe2014@gmail.com>
2026-02-20 09:50:50 +0700

    Update useBooking.js

--- 17-The Wild Oasis/src/features/bookings/useBooking.js
+++ 17-The Wild Oasis/src/features/bookings/useBooking.js
@@ -5,10 +5,12 @@ export function useBooking(){
     const [searchParams]= useSearchParams()
     // filter 
     const fillterValue = searchParams.get("status")
-    const filter = !fillterValue || fillterValue==="all"?null:{field:"status",value: fillterValue}
-  
+    const filter = !fillterValue || fillterValue==="all"?null:
+     {field:"status",value: fillterValue} 
+    //{field:"totalPrice",value: 5000,method:"gte"}  
   
     const {data:Booking,isLoading}=useQuery({
+        // filter is independency , it refresh when update value
         queryKey:["bookings",filter],
         queryFn: ()=>getBookings({filter})
 
