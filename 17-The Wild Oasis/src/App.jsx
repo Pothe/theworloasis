c633da27c2068b7593f647d7a0c3cef511fc2940
Pothe <keoutpothe2014@gmail.com>
2026-02-28 10:28:15 +0700

    Update App.jsx

--- 17-The Wild Oasis/src/App.jsx
+++ 17-The Wild Oasis/src/App.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Dashboard from './pages/Dashboard'
 import PageNotFound from './pages/PageNotFound'
 import Account from './pages/Account'
-import Booking from './pages/Bookings'
+import Bookings from './pages/Bookings'
 import Cabin from './pages/Cabins'
 import Login from './pages/Login'
 import User from './pages/Users'
@@ -44,7 +44,7 @@ function App() {
         <Route index  element={<Navigate replace to="/dashboard"/>}/>
         <Route path="dashboard" element={<Dashboard/>}/>
         <Route path="/account" element={<Account/>}/>
-        <Route path="/booking" element={<Booking/>}/>  
+        <Route path="/booking" element={<Bookings/>}/>  
         <Route path="/booking/bookings/:bookingId" element={<BookingDetail/>}/>  
         <Route path="/cabins" element={<Cabin/>}/>
         <Route path="/setting" element={<Setting/>}/>
